97fc4334700eb2b4d808fd9b60bb07c02aab2bf0
lxj <1176891456@qq.com>
2024-08-06 22:08:30 +0800

    chore: vercel.json

--- app/code-fun/page.tsx
+++ app/code-fun/page.tsx
@@ -6,23 +6,25 @@ import Link from 'next/link';
 const CodeFunPage = () => {
   return (
     <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
-      {allCodeFuns.map((e) => (
-        <Link key={e._id} href={`/code-fun/${e.slugAsParams}`}>
-          <Card className="overflow-hidden">
-            <div className="relative pb-[58%]">
-              <Image
-                src={`${e.slug}.png`}
-                fill
-                alt={e.title}
-                className="object-cover"
-                sizes="512px"
-              />
-            </div>
+      {allCodeFuns
+        .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf())
+        .map((e) => (
+          <Link key={e._id} href={`/code-fun/${e.slugAsParams}`}>
+            <Card className="overflow-hidden">
+              <div className="relative pb-[58%]">
+                <Image
+                  src={`${e.slug}.png`}
+                  fill
+                  alt={e.title}
+                  className="object-cover"
+                  sizes="512px"
+                />
+              </div>
 
-            <h2 className="p-2 text-slate-500 text-base">{e.title}</h2>
-          </Card>
-        </Link>
-      ))}
+              <h2 className="p-2 text-slate-500 text-base">{e.title}</h2>
+            </Card>
+          </Link>
+        ))}
     </div>
   );
 };
