bad5344a78a4b7d1f662cda17ecdc0bad4255638
lxj <1176891456@qq.com>
2024-08-24 19:13:38 +0800

    feat:摆钟

--- components/baidu-analytics.tsx
+++ components/baidu-analytics.tsx
@@ -2,15 +2,17 @@ import Script from 'next/script';
 
 const BaiduAnalytics = () => {
   return (
-    <Script id="bdtj">
-      {`var _hmt = _hmt || [];
+    process.env.NODE_ENV === 'production' && (
+      <Script id="bdtj">
+        {`var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?814cf9007574befd36723c7528f1190f";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();`}
-    </Script>
+      </Script>
+    )
   );
 };
 
