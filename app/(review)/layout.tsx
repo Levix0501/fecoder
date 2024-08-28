de21bc35d7c3c0c92cefbf921c940cbee1019e1b
lxj <1176891456@qq.com>
2024-08-28 16:35:03 +0800

    feat: waline

---
+++ app/(review)/layout.tsx
@@ -0,0 +1,10 @@
+import { Waline } from '@/components/waline/waline';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <Waline />
+    </>
+  );
+}
--- components/waline/waline.tsx
+++ components/waline/waline.tsx
@@ -5,12 +5,15 @@ import {
   type WalineInitOptions,
   init,
 } from '@waline/client';
-
+import styles from './styles.module.scss';
 import '@waline/client/style';
+import { usePathname } from 'next/navigation';
 
-export type WalineOptions = Omit<WalineInitOptions, 'el'> & { path: string };
+export type WalineOptions = Omit<WalineInitOptions, 'el' | 'serverURL'> & {
+  path: string;
+};
 
-export const Waline = (props: WalineOptions) => {
+const _Waline = (props: WalineOptions) => {
   const walineInstanceRef = useRef<WalineInstance | null>(null);
   const containerRef = React.createRef<HTMLDivElement>();
 
@@ -24,11 +27,21 @@ export const Waline = (props: WalineOptions) => {
     });
 
     return () => walineInstanceRef.current?.destroy();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     walineInstanceRef.current?.update(props);
   }, [props]);
 
-  return <div ref={containerRef} />;
+  return (
+    <div className="mt-10">
+      <div ref={containerRef} className={styles['waline']} />
+    </div>
+  );
+};
+
+export const Waline = () => {
+  const pathname = usePathname().replace('/doc', '');
+  return <_Waline path={pathname} />;
 };
